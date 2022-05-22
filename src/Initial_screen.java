a9871bce09e5e8baa2e5610080aa9f537f564dc1
bgs-study <qkrwlgp235@gmail.com>
2022-05-22 23:17:50 +0900

    indent rearrange

--- src/Initial_screen.java
+++ src/Initial_screen.java
@@ -4,16 +4,12 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,80 +17,80 @@ import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 
 
-public class Initial_screen extends JFrame {
+class Initial_screen extends JFrame {
+	
 	public Initial_screen() { //생성자
+		
 		setTitle("초기 화면");
-	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫으면 프로그램 종료
-	      Container c = getContentPane();
-	      c.setLayout(new BorderLayout());
-
-	   //JPanner 팝업창
-	      
-	      //BorderLayout.CENTER
-	      JPanel p_background = new JPanel(new GridLayout(0,3));      
-	      JScrollPane p_center = new JScrollPane(p_background,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-	      p_background.setBackground(Color.white);
-	      c.add(p_center,BorderLayout.CENTER);
-	      
-	      
-	      //BorderLayout.NORTH
-	      JPanel p_north = new JPanel(new FlowLayout());
-	      p_north.setBackground(new Color(255, 158, 60));
-	      c.add(p_north,BorderLayout.NORTH);
-	      
-	      //BorderLayout.SOUTH
-	      JPanel p_south = new JPanel(new FlowLayout());         
-	      p_south.setBackground(new Color(255, 158, 60));      
-	      c.add(p_south,BorderLayout.SOUTH);
-	      
-	      //p_background
-	      JLabel screen = new JLabel (new ImageIcon("https://user-images.githubusercontent.com/101086248/169693778-2020e5f3-7a63-41a4-808a-844043bab2ba.png"));
-	      p_background.add(screen);
-	      
-	      //p_north title
-	      JLabel logo = new JLabel(new ImageIcon("./images/img/logo_transparent.png"));     
-	      p_north.add(logo);
-
-	      
-	      
-	      
-	      //매장식사, 포장 버튼
-	      JToggleButton eatin_btn = new JToggleButton("매장 식사");
-	      eatin_btn.setBounds(0, 665, 300, 100); //위치
-	      eatin_btn.setBackground(new Color(255, 158, 60)); //글자 색
-	      eatin_btn.setForeground(Color.white); // 배경 색
-	      eatin_btn.setBorder(BorderFactory.createLineBorder(Color.white)); //버튼 테두리 색
-	      eatin_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
-	      p_south.add(eatin_btn, BorderLayout.WEST);
-	      
-	      JButton eatout_btn = new JButton("포장 하기");
-	      eatout_btn.setBounds(300, 665, 300, 100);//위치
-	      eatout_btn.setBackground(new Color(255, 158, 60));//글자 색
-	      eatout_btn.setForeground(Color.white); // 배경 색
-	      eatout_btn.setBorder(BorderFactory.createLineBorder(Color.white));//버튼 테두리 색
-	      eatout_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
-	      p_south.add(eatout_btn,BorderLayout.EAST);	   
-	      
-	      
-	      //이벤트: 버튼 누르면 메뉴 고르는 화면으로
-	      Menu m = new Menu();
-	      
-	      eatin_btn.addActionListener(new ActionListener() {
-	          public void actionPerformed(ActionEvent e) {	      
-	        	  m.setVisible(true); //메뉴창 열고
-	        	  setVisible(false); // 초기화면 닫고
-	          }
-	      });
-	      
-	      eatout_btn.addActionListener(new ActionListener() {
-	          public void actionPerformed(ActionEvent e) {
-	        	  m.setVisible(true); //메뉴창 열고
-	        	  setVisible(false); // 초기화면 닫고
-	          }
-	       });      	      
-	      
-	   // 프레임 크기 
-			setSize(600, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫으면 프로그램 종료
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		//JPanner 팝업창
+		  
+		//BorderLayout.CENTER
+		JPanel p_background = new JPanel(new GridLayout(0,3));      
+		JScrollPane p_center = new JScrollPane(p_background,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		p_background.setBackground(Color.white);
+		c.add(p_center,BorderLayout.CENTER);
+		  
+		  
+		//BorderLayout.NORTH
+		JPanel p_north = new JPanel(new FlowLayout());
+		p_north.setBackground(new Color(255, 158, 60));
+		c.add(p_north,BorderLayout.NORTH);
+		  
+		//BorderLayout.SOUTH
+		JPanel p_south = new JPanel(new FlowLayout());         
+		p_south.setBackground(new Color(255, 158, 60));      
+		c.add(p_south,BorderLayout.SOUTH);
+		  
+		//p_background
+		JLabel screen = new JLabel (new ImageIcon("https://user-images.githubusercontent.com/101086248/169693778-2020e5f3-7a63-41a4-808a-844043bab2ba.png"));
+		p_background.add(screen);
+		  
+		//p_north title
+		JLabel logo = new JLabel(new ImageIcon("./images/img/logo_transparent.png"));     
+		p_north.add(logo);
+		
+		  
+		  
+		//매장식사, 포장 버튼
+		JToggleButton eatin_btn = new JToggleButton("매장 식사");
+		eatin_btn.setBounds(0, 665, 300, 100); //위치
+		eatin_btn.setBackground(new Color(255, 158, 60)); //글자 색
+		eatin_btn.setForeground(Color.white); // 배경 색
+		eatin_btn.setBorder(BorderFactory.createLineBorder(Color.white)); //버튼 테두리 색
+		eatin_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
+		p_south.add(eatin_btn, BorderLayout.WEST);
+		  
+		JButton eatout_btn = new JButton("포장 하기");
+		eatout_btn.setBounds(300, 665, 300, 100);//위치
+		eatout_btn.setBackground(new Color(255, 158, 60));//글자 색
+		eatout_btn.setForeground(Color.white); // 배경 색
+		eatout_btn.setBorder(BorderFactory.createLineBorder(Color.white));//버튼 테두리 색
+		eatout_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
+		p_south.add(eatout_btn,BorderLayout.EAST);	   
+		  
+		  
+		//이벤트: 버튼 누르면 메뉴 고르는 화면으로
+//		Menu m = new Menu();
+//		  
+//		eatin_btn.addActionListener(new ActionListener() {
+//		      public void actionPerformed(ActionEvent e) {	      
+//		    	  m.setVisible(true); //메뉴창 열고
+//		    	  setVisible(false); // 초기화면 닫고
+//		      }
+//		  });
+//		  
+//		eatout_btn.addActionListener(new ActionListener() {
+//		      public void actionPerformed(ActionEvent e) {
+//		    	  m.setVisible(true); //메뉴창 열고
+//		    	  setVisible(false); // 초기화면 닫고
+//		      }
+//		   });      	      
+			      
+	    // 프레임 크기 
+		setSize(600, 800);
 	      
 	}
 	 
